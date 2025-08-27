7d0d4f48446743a9c8b949b387f8fb5e9e9b4789
Akshat Shah <akshatshah1605@gmail.com>
2025-08-27 13:28:37 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (64.77%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -1,10 +1,8 @@
-from collections import deque
-
 class HitCounter:
 
     def __init__(self):
         self.hc = deque()
-        self.WINDOW = 300
+        self.WINDOW = 300        
 
     def hit(self, timestamp: int) -> None:
         if self.hc and self.hc[-1][0] == timestamp:
@@ -13,19 +11,17 @@ class HitCounter:
         else:
             self.hc.append([timestamp, 1])
         
-        print(self.hc)
-        
 
     def getHits(self, timestamp: int) -> int:
         totalHits = 0
-        while self.hc and self.hc[0][0] <= timestamp - self.WINDOW:
+        minLookBackTime = timestamp - self.WINDOW
+        while self.hc and self.hc[0][0] <= minLookBackTime:
             self.hc.popleft()
         
         for timestamp, count in self.hc:
             totalHits += count
-
+        
         return totalHits
-            
         
 
 
