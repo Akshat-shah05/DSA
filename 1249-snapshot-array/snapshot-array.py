ed8e032eb28293de73101d3ce429263439f4e1c2
Akshat Shah <akshatshah1605@gmail.com>
2025-08-27 22:51:53 -0400

    Time: 182 ms (65.47%) | Memory: 44.9 MB (62.59%) - LeetSync

--- 1249-snapshot-array/snapshot-array.py
+++ 1249-snapshot-array/snapshot-array.py
@@ -1,23 +1,23 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.snapId = -1
+        self.nextSid = 0
         self.snapArray = [[(-1, 0)] for _ in range(length)]
         
-
     def set(self, index: int, val: int) -> None:
         arr = self.snapArray[index]
-        if arr[-1][0] == self.snapId + 1:
-            arr[-1] = (self.snapId + 1, val)
+        if arr[-1][0] == self.nextSid:
+            arr[-1] = (self.nextSid, val)
         
         else:
-            arr.append((self.snapId + 1, val))
-
-    def snap(self) -> int:
-        self.snapId += 1
-        return self.snapId
+            arr.append((self.nextSid, val))
         
 
+    def snap(self) -> int:
+        sid = self.nextSid
+        self.nextSid += 1
+        return sid
+        
     def get(self, index: int, snap_id: int) -> int:
         arr = self.snapArray[index]
         l = 0
@@ -30,7 +30,6 @@ class SnapshotArray:
                 r = mid - 1
         
         return arr[r][1]
-        
 
 
 # Your SnapshotArray object will be instantiated and called as such:
