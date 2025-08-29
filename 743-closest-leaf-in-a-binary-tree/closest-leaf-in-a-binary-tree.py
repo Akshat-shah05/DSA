b2b53b2bfa5088b0d87f054e7ae3e274aff7138b
Akshat Shah <akshatshah1605@gmail.com>
2025-08-29 14:36:02 -0400

    Time: 7 ms (40.82%) | Memory: 18.8 MB (37.53%) - LeetSync

--- 743-closest-leaf-in-a-binary-tree/closest-leaf-in-a-binary-tree.py
+++ 743-closest-leaf-in-a-binary-tree/closest-leaf-in-a-binary-tree.py
@@ -7,33 +7,36 @@
 class Solution:
     def findClosestLeaf(self, root: Optional[TreeNode], k: int) -> int:
         graph = defaultdict(list)
-        q = deque([root])
         start = None
 
-        while q:
-            node = q.popleft()
+        def dfs(node, parent = None):
+            nonlocal start
+            if not node:
+                return 
+            
             if node.val == k:
                 start = node
             
-            if node.left:
-                graph[node.left].append(node)
-                graph[node].append(node.left)
-                q.append(node.left)
-            
-            if node.right:
-                graph[node.right].append(node)
-                graph[node].append(node.right)
-                q.append(node.right)
+            graph[node].append(parent)
+            graph[parent].append(node)
+            dfs(node.left, node)
+            dfs(node.right, node)
+        
+        dfs(root)
         
         q2 = deque([start])
         seen = set()
         while q2:
             node = q2.popleft()
-            if not node.left and not node.right:
-                return node.val
-            
-            seen.add(node)
-            
+            if node:
+                if not node.left and not node.right:
+                    return node.val
+                
+                for nei in graph[node]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        q2.append(nei)
+                        
             for nei in graph[node]:
                 if nei not in seen:
                     q2.append(nei)
