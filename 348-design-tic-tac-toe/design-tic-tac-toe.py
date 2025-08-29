c29203e753e3a0d7f111f58424527b04aa185146
Akshat Shah <akshatshah1605@gmail.com>
2025-08-29 13:26:53 -0400

    Time: 85 ms (29.69%) | Memory: 20.6 MB (9.86%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/design-tic-tac-toe.py
@@ -1,48 +1,40 @@
 class TicTacToe:
 
-    def __init__(self, n: int) -> None:
+    def __init__(self, n: int):
         self.n = n
         self.board = [[None] * n for _ in range(n)]
         self.directions = [(1, 0), (0, 1), (1, 1), (-1, 1)]
 
     def move(self, row: int, col: int, player: int) -> int:
         if self.board[row][col] is not None:
-            raise ValueError("Cell already taken, try again!")
+            raise ValueError("This cell is already taken")
         
         self.board[row][col] = player
         return player if self._checkwin(row, col, player) else 0
-
     
-    def _checkwin(self, row: int, col: int, player: int) -> bool:
+    def _checkwin(self, row, col, player):
         for dr, dc in self.directions:
-            count = -1
-
-            # Moving in the forward direction
-            newRow, newCol = row, col
-            while self._validRC(newRow, newCol, player):
-                count += 1
-                newRow += dr
-                newCol += dc
+            count = 1
             
-            # Moving in the backwards direction
-            newRow, newCol = row, col
-            while self._validRC(newRow, newCol, player):
+            # Forward Direction
+            newR, newC = row + dr, col + dc
+            while 0 <= newR < self.n and 0 <= newC < self.n and self.board[newR][newC] == player:
                 count += 1
-                newRow -= dr
-                newCol -= dc
+                newR += dr
+                newC += dc
+        
+            # Reverse Direction
+            newR, newC = row - dr, col - dc
+            while 0 <= newR < self.n and 0 <= newC < self.n and self.board[newR][newC] == player:
+                count += 1
+                newR -= dr
+                newC -= dc
             
             if count >= self.n:
                 return True
         
         return False
-    
-    def _validRC(self, row, col, player):
-        return 0 <= row < self.n and 0 <= col < self.n and self.board[row][col] == player
-            
-
         
-
-
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
 # param_1 = obj.move(row,col,player)
