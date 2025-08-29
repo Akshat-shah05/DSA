86247451c34e36e534ad22bc191abbd10d17129a
Akshat Shah <akshatshah1605@gmail.com>
2025-08-29 14:53:51 -0400

    Time: 211 ms (43.03%) | Memory: 49.1 MB (19.09%) - LeetSync

--- 1249-snapshot-array/snapshot-array.py
+++ 1249-snapshot-array/snapshot-array.py
@@ -1,35 +1,39 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.nextSid = 0
-        self.snapArray = [[(-1, 0)] for _ in range(length)]
-        
+        self.snapId = 0
+        self.snapArray = [[[-1, 0]] for _ in range(length)]
+
     def set(self, index: int, val: int) -> None:
-        arr = self.snapArray[index]
-        if arr[-1][0] == self.nextSid:
-            arr[-1] = (self.nextSid, val)
+        if self.snapArray[index][-1][0] == self.snapId:
+            self.snapArray[index][-1][1] = val
         
         else:
-            arr.append((self.nextSid, val))
+            self.snapArray[index].append([self.snapId, val])
         
-
     def snap(self) -> int:
-        sid = self.nextSid
-        self.nextSid += 1
-        return sid
+        snap = self.snapId
+        self.snapId += 1
+        return snap
         
     def get(self, index: int, snap_id: int) -> int:
         arr = self.snapArray[index]
         l = 0
         r = len(arr) - 1
+
         while l <= r:
             mid = (l + r) // 2
-            if arr[mid][0] <= snap_id:
+            if arr[mid][0] == snap_id:
+                return arr[mid][1]
+            
+            elif arr[mid][0] < snap_id:
                 l = mid + 1
+            
             else:
                 r = mid - 1
         
         return arr[r][1]
+        
 
 
 # Your SnapshotArray object will be instantiated and called as such:
