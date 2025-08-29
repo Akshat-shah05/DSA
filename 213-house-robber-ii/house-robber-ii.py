373fc07257993475ce168e57e7ecb868295ac4ed
Akshat Shah <akshatshah1605@gmail.com>
2025-08-29 14:12:33 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.51%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,21 +1,19 @@
 class Solution:
-    def robHelper(self, nums: List[int]) -> int:
+    def rob(self, nums: List[int]) -> int:
         if not nums:
             return 0
         
         if len(nums) == 1:
             return nums[0]
-        
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-        
-        return dp[-1]
-    
-    def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+            
         return max(self.robHelper(nums[1:]), self.robHelper(nums[:-1]))
+    
+    def robHelper(self, nums):
+        prev1, prev2 = 0, 0
+        for num in nums:
+            temp = max(prev2 + num, prev1)
+            prev2 = prev1
+            prev1 = temp
+        
+        return prev1
+        
