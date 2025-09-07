71f67716a3d2f05ed0a66d9566d967634eb88095
Akshat Shah <akshatshah1605@gmail.com>
2025-09-06 20:37:13 -0400

    Time: 148 ms (32.49%) | Memory: 35.6 MB (74.63%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -3,17 +3,17 @@ class Solution:
         freq = defaultdict(int)
         pairs = 0
         centre = 0
+
         for word in words:
             if freq[word[::-1]] > 0:
                 freq[word[::-1]] -= 1
                 pairs += 1
+            
             else:
                 freq[word] += 1
         
         for word, count in freq.items():
-            if word[0] == word[1]:
-                pairs += count//2
-                if count % 2 == 1:
-                    centre = 1
+            if word[0] == word[1] and count % 2 == 1:
+                centre = 1
         
         return pairs * 4 + centre * 2
