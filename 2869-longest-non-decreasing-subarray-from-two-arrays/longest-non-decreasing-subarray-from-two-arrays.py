e99e54f4c0042c5afe2f969c6eaa7ce23482b325
Akshat Shah <akshatshah1605@gmail.com>
2025-09-25 02:56:28 -0400

    Time: 288 ms (27.15%) | Memory: 44.6 MB (27.90%) - LeetSync

--- 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.py
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.py
@@ -5,8 +5,6 @@ class Solution:
         dp = [[1, 1] for _ in range(len(nums1))]
         ans = 1
         for i in range(1, n):
-            dp[i][0] = 1
-            dp[i][1] = 1
 
             if nums1[i] >= nums1[i - 1]:
                 dp[i][0] = max(dp[i][0], dp[i - 1][0] + 1)
