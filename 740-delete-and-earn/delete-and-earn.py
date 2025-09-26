eff137070bdea9ef790f5a15eab9238fc18dc156
Akshat Shah <akshatshah1605@gmail.com>
2025-09-26 00:37:44 -0400

    Time: 16 ms (19.40%) | Memory: 20 MB (33.85%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.py
@@ -0,0 +1,15 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        points = defaultdict(int)
+        maxVal = 0
+        for num in nums:
+            points[num] += num
+            maxVal = max(maxVal, num)
+        
+        dp = [0] * (maxVal + 1)
+        dp[1] = points[1]
+
+        for i in range(2, maxVal + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + points[i])
+        
+        return dp[-1]
