ef1177d7abdd52dc3eb10e7a71cba25b1da67a3c
Akshat Shah <akshatshah1605@gmail.com>
2025-09-27 17:21:03 -0400

    Time: 10 ms (48.39%) | Memory: 19.5 MB (74.49%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -7,10 +7,8 @@ class Solution:
             maxVal = max(num, maxVal)
         
         prev2, prev1 = points[0], points[1]
-        curr = 0
 
         for i in range(2, maxVal + 1):
-            curr = max(prev2 + points[i], prev1)
-            prev2, prev1 = prev1, curr
+            prev2, prev1 = prev1, max(prev2 + points[i], prev1)
         
         return prev1
