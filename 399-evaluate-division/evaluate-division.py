c586360bd19101e0c8d57b84358c8b9a31f86e7d
Akshat Shah <akshatshah1605@gmail.com>
2025-09-27 16:31:55 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (93.45%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -7,28 +7,26 @@ class Solution:
             adjList[A].append((B, values[i]))
             adjList[B].append((A, 1/values[i]))
         
-        ans = []
-
         def dfs(start, end, result, visited):
             if start not in adjList or end not in adjList:
                 return -1.0
-
+            
             if start == end:
                 return result
             
             visited.add(start)
-            
+
             for nei, weight in adjList[start]:
                 if nei in visited:
                     continue
-
-                res = dfs(nei, end, result * weight, visited)
-                if res != -1.0:
-                    return res
+                
+                ans = dfs(nei, end, result * weight, visited)
+                if ans != -1.0:
+                    return ans
             
             return -1.0
-
-
+        
+        ans = []
 
         for C, D in queries:
             ans.append(dfs(C, D, 1, set()))
