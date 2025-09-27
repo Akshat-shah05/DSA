602237cf23e3090dd17756ce7662cb28b52aa3eb
Akshat Shah <akshatshah1605@gmail.com>
2025-09-27 17:09:24 -0400

    Time: 228 ms (41.01%) | Memory: 44.7 MB (24.07%) - LeetSync

--- 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.py
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.py
@@ -1,29 +1,20 @@
 class Solution:
     def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
-        # check if nums1[i] >= nums1[i - 1] and nums1[i] >= nums2[i - 1]
-        # check if nums2[i] >= nums2[i - 1] and nums2[i] >= nums1[i - 1]
-
-        # dp[i][0] --> would represent longest nondecreasing subarray, given you choose nums1[i]
-        # dp[i][1] --> would represent longest nondecreasing subarray, given you choose nums2[i]
-
         dp = [[1, 1] for _ in range(len(nums1))]
         ans = 1
-        a = 1
-        b = 1
 
         for i in range(1, len(nums1)):
-            na, nb = 1, 1
-
             if nums1[i] >= nums1[i - 1]:
-                na = a + 1
-            if nums1[i] >= nums2[i - 1]:
-                na = max(na, b + 1)
-            if nums2[i] >= nums2[i - 1]:
-                nb = b + 1
-            if nums2[i] >= nums1[i - 1]:
-                nb = max(a + 1, nb)
+                dp[i][0] = dp[i - 1][0] + 1
             
-            a, b = na, nb
-            ans = max(ans, a, b)
-        
+            if nums1[i] >= nums2[i - 1]:
+                dp[i][0] = max(dp[i][0], dp[i - 1][1] + 1)
+            
+            if nums2[i] >= nums2[i - 1]:
+                dp[i][1] = dp[i - 1][1] + 1
+            
+            if nums2[i] >= nums1[i - 1]:
+                dp[i][1] = max(dp[i][1], dp[i - 1][0] + 1)
+
+            ans = max(ans, dp[i][0], dp[i][1])
         return ans
