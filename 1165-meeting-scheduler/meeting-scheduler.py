449299ae0748e53112c59bdeef5297c0054fe890
Akshat Shah <akshatshah1605@gmail.com>
2025-09-27 21:17:22 -0400

    Time: 71 ms (34.69%) | Memory: 23.5 MB (37.80%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.py
+++ 1165-meeting-scheduler/meeting-scheduler.py
@@ -2,15 +2,14 @@ class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
         slots1.sort()
         slots2.sort()
-
         s1 = s2 = 0
 
         while s1 < len(slots1) and s2 < len(slots2):
-            intersect_start = max(slots1[s1][0], slots2[s2][0])
-            intersect_end = min(slots1[s1][1], slots2[s2][1])
+            start = max(slots1[s1][0], slots2[s2][0])
+            end = min(slots1[s1][1], slots2[s2][1])
 
-            if intersect_start + duration <= intersect_end:
-                return [intersect_start, intersect_start + duration]
+            if start + duration <= end:
+                return [start, start + duration]
             
             if slots1[s1][1] >= slots2[s2][1]:
                 s2 += 1
