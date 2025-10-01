d446d438f678c117950f906255a720257d2f3aa3
Akshat Shah <akshatshah1605@gmail.com>
2025-10-01 00:40:12 -0400

    Time: 10 ms (48.42%) | Memory: 19.8 MB (41.09%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -4,17 +4,12 @@ class Solution:
         for num in nums:
             points[num] += num
         
-        elements = sorted(points.keys())
-        prev2 = 0
-        prev1 = points[elements[0]]
+        maxVal = max(nums)
+        dp = [0] * (maxVal + 1)
+        dp[0] = 0
+        dp[1] = points[1]
 
-        for i in range(1, len(elements)):
-            cur_elem = elements[i]
-            if cur_elem == elements[i - 1] + 1:
-                # Then they are adjacent
-                prev2, prev1 = prev1, max(prev1, prev2 + points[cur_elem])
-            
-            else:
-                prev2, prev1 = prev1, prev1 + points[cur_elem]
+        for i in range(2, maxVal + 1):
+            dp[i] = max(dp[i - 2] + points[i], dp[i - 1])
         
-        return prev1
+        return dp[-1]
