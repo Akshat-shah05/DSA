322339fd72574d1defeab9fa9a155e9cc5e31e96
Akshat Shah <akshatshah1605@gmail.com>
2025-10-01 00:39:16 -0400

    Time: 7 ms (79.76%) | Memory: 19.7 MB (48.33%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -1,14 +1,20 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         points = defaultdict(int)
-        maxVal = 0
         for num in nums:
             points[num] += num
-            maxVal = max(num, maxVal)
         
-        prev2, prev1 = points[0], points[1]
+        elements = sorted(points.keys())
+        prev2 = 0
+        prev1 = points[elements[0]]
 
-        for i in range(2, maxVal + 1):
-            prev2, prev1 = prev1, max(prev2 + points[i], prev1)
+        for i in range(1, len(elements)):
+            cur_elem = elements[i]
+            if cur_elem == elements[i - 1] + 1:
+                # Then they are adjacent
+                prev2, prev1 = prev1, max(prev1, prev2 + points[cur_elem])
+            
+            else:
+                prev2, prev1 = prev1, prev1 + points[cur_elem]
         
         return prev1
