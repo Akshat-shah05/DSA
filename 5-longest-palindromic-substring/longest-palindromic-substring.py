e17faecbacc0c49e872d78fecc854267b832f5a4
Akshat Shah <akshatshah1605@gmail.com>
2025-10-20 21:41:56 -0400

    Time: 198 ms (94.82%) | Memory: 17.8 MB (58.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,27 +4,34 @@ class Solution:
         if n == 0:
             return ""
         
-        dp = [[None] * n for _ in range(n)]
-        ans = [0, 0]
+        max_start = 0
+        max_length = 1
 
         for i in range(n):
-            dp[i][i] = True
-        
-        for i in range(n - 1):
-            dp[i][i + 1] = True if s[i] == s[i + 1] else False
-            ans = [i, i + 1] if dp[i][i + 1] else ans
-        
-        for sLen in range(3, n + 1):
-            i = 0
-            for j in range(sLen + i - 1, n):
-                dp[i][j] = dp[i + 1][j - 1] and s[i] == s[j]
-                if dp[i][j]:
-                    ans = [i, j]
-                
-                i += 1
-        
-        i, j = ans
-        return s[i : j + 1]
-        
+            l = r = i
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            
+            l += 1
+            r -= 1
+            
+            if (r - l + 1) > max_length:
+                max_length = (r - l + 1)
+                max_start = l
 
+            l = i
+            r = i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            
+            l += 1
+            r -= 1
 
+            if (r - l + 1) > max_length:
+                max_length = (r - l + 1)
+                max_start = l
+        
+        print(max_length)
+        return s[max_start : max_start + max_length]
