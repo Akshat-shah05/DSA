cbf38c0ef1942911eeca1534f31ffd02ab43c0f1
Akshat Shah <akshatshah1605@gmail.com>
2025-11-01 14:14:10 -0700

    Time: 47 ms (61.52%) | Memory: 32.3 MB (42.33%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,49 +1,50 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.endOfWord = False
+        self.EOW = False
 
 class Trie:
 
     def __init__(self):
         self.root = TrieNode()
         
-    def insert(self, word: str) -> None:
-        curNode = self.root
-        
-        for char in word:
-            if char not in curNode.children:
-                curNode.children[char] = TrieNode()
-            
-            curNode = curNode.children[char]
 
-        curNode.endOfWord = True
+    def insert(self, word: str) -> None:
+        root = self.root
+        for char in word:
+            if char not in root.children:
+                root.children[char] = TrieNode()
+            
+            root = root.children[char]
+        
+        root.EOW = True
+
 
     def search(self, word: str) -> bool:
-        def dfs(node, index):
-            if index == len(word):
-                return node.endOfWord
+        def dfs(node, idx):
+            if idx == len(word):
+                return node.EOW
             
-            char = word[index]
+            char = word[idx]
             if char not in node.children:
                 return False
+            
+            return dfs(node.children[char], idx + 1)
 
-            return dfs(node.children[char], index + 1)
-        
-        return dfs(self.root, 0)        
+        return dfs(self.root, 0)
 
     def startsWith(self, prefix: str) -> bool:
-        def dfs(node, index):
-            if index == len(prefix):
+        def dfs(node, idx):
+            if idx == len(prefix):
                 return True
             
-            char = prefix[index]
+            char = prefix[idx]
             if char not in node.children:
                 return False
+            
+            return dfs(node.children[char], idx + 1)
 
-            return dfs(node.children[char], index + 1)
-        
-        return dfs(self.root, 0)     
+        return dfs(self.root, 0)
         
 
 
@@ -52,3 +53,4 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
