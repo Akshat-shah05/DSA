ee155194fa4ade9cfb8230370cdd2cc908a7866c
Akshat Shah <akshatshah1605@gmail.com>
2025-11-03 23:12:41 -0800

    Time: 3720 ms (64.41%) | Memory: 18.1 MB (21.54%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,31 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ans = False
-        n = len(word)
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         rows, cols = len(board), len(board[0])
+        n = len(word)
     
         def backtrack(r, c, i):
             if i == n:
                 return True
-            
-            if not (0 <= r < rows and 0 <= c < cols) or board[r][c] != word[i]:
-                return False
 
+            if not 0 <= r < rows or not 0 <= c < cols or word[i] != board[r][c]:
+                return False
+            
             ch = board[r][c]
             board[r][c] = "#"
-            found = (
-                backtrack(r + 1, c, i + 1) or
-                backtrack(r - 1, c, i + 1) or 
-                backtrack(r, c + 1, i + 1) or
-                backtrack(r, c - 1, i + 1)
-            )
+
+            for dr, dc in directions:
+                if backtrack(r + dr, c + dc, i + 1):
+                    return True
+            
             board[r][c] = ch
-            return found
+            return False
+        
 
         for r in range(rows):
             for c in range(cols):
-                if backtrack(r, c, 0):
+                if board[r][c] == word[0] and backtrack(r, c, 0):
                     return True
-
+            
         return False
-    
