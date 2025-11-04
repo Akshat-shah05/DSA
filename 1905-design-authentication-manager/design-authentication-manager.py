0451516d27652e667694b4420b541a9dd04a7b4c
Akshat Shah <akshatshah1605@gmail.com>
2025-11-03 20:04:55 -0800

    Time: 74 ms (42.17%) | Memory: 19.6 MB (15.56%) - LeetSync

--- 1905-design-authentication-manager/design-authentication-manager.py
+++ 1905-design-authentication-manager/design-authentication-manager.py
@@ -2,33 +2,32 @@ class AuthenticationManager:
 
     def __init__(self, timeToLive: int):
         self.ttl = timeToLive
-        self.exp = {} # -> token -> Expiry time
-        self.heap = [] # -> heap of (expiry, token)
-    
-    def _evict(self, currentTime):
-        while self.heap and self.heap[0][0] <= currentTime:
-            expiry, tok = heapq.heappop(self.heap)
-
-            if self.exp.get(tok) == expiry:
-                del self.exp[tok]
-        
+        self.tokenMap = {}
 
     def generate(self, tokenId: str, currentTime: int) -> None:
-        self._evict(currentTime)
-        self.exp[tokenId] = currentTime + self.ttl
-        heapq.heappush(self.heap, (currentTime + self.ttl, tokenId))
+        self.tokenMap[tokenId] = self.ttl + currentTime
 
     def renew(self, tokenId: str, currentTime: int) -> None:
-        self._evict(currentTime)
-        if tokenId not in self.exp:
-            return None
+        if tokenId not in self.tokenMap:
+            return 
         
-        self.exp[tokenId] = currentTime + self.ttl
-        heapq.heappush(self.heap, (currentTime + self.ttl, tokenId))
+        if self.tokenMap[tokenId] <= currentTime:
+            del self.tokenMap[tokenId]
+            return 
+        
+        self.tokenMap[tokenId] = currentTime + self.ttl
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        self._evict(currentTime)
-        return len(self.exp)
+        ans = 0
+        allItems = list(self.tokenMap.items()).copy()
+        for token, expiry in allItems:
+            if expiry <= currentTime:
+                del self.tokenMap[token]
+            
+            else:
+                ans += 1
+        
+        return ans
         
 
 
