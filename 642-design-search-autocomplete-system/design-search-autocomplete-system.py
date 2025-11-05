aaa4996c155e922e3be48d319d3f9477f59a6988
Akshat Shah <akshatshah1605@gmail.com>
2025-11-04 19:17:38 -0800

    Time: 145 ms (83.40%) | Memory: 24.2 MB (47.86%) - LeetSync

--- 642-design-search-autocomplete-system/design-search-autocomplete-system.py
+++ 642-design-search-autocomplete-system/design-search-autocomplete-system.py
@@ -1,49 +1,50 @@
 class TrieNode:
     def __init__(self):
-        self.children = {} # --> char -> TrieNode
-        self.sentences = defaultdict(int) # --> Sentence -> count
+        self.children = {}
+        self.sentences = defaultdict(int)
 
 class AutocompleteSystem:
 
     def __init__(self, sentences: List[str], times: List[int]):
         self.root = TrieNode()
-        for sentence, count in zip(sentences, times):
-            self.add_to_trie(sentence, count)
+
+        for (sentence, count) in zip(sentences, times):
+            self._add_to_trie(sentence, count)
         
-        self.curr_sentence = []
-        self.curr_node = self.root
+        self.curSent = []
+        self.curNode = self.root
         self.dead = TrieNode()
 
     def input(self, c: str) -> List[str]:
         if c == "#":
-            full_sentence = "".join(self.curr_sentence)
-            self.add_to_trie(full_sentence, 1)
-            self.curr_sentence = []
-            self.curr_node = self.root
+            curSentence = "".join(self.curSent)
+            self._add_to_trie(curSentence, 1)
+            self.curSent = []
+            self.curNode = self.root
             return []
         
-        if c not in self.curr_node.children:
-            self.curr_sentence.append(c)
-            self.curr_node = self.dead
+        self.curSent.append(c)
+        if c not in self.curNode.children:
+            self.curNode = self.dead
             return []
         
-        self.curr_node = self.curr_node.children[c]
-        self.curr_sentence.append(c)
-        sentences = [(val, key) for key, val in self.curr_node.sentences.items()]
-        sentences.sort(key = lambda x: (-x[0], x[1]))
+        self.curNode = self.curNode.children[c]
+        sentences = self.curNode.sentences
+        sorted_sentences = sorted(sentences.items(), key=lambda x: (-x[1], x[0]))
+
         ans = []
-        for i in range(min(3, len(sentences))):
-            ans.append(sentences[i][1])
+
+        for i in range(min(3, len(sorted_sentences))):
+            ans.append(sorted_sentences[i][0])
         
         return ans
 
-    def add_to_trie(self, sentence, count):
+    def _add_to_trie(self, sentence, count):
         root = self.root
-        for c in sentence:
-            if c not in root.children:
-                root.children[c] = TrieNode()
-            
-            root = root.children[c]
+        for ch in sentence:
+            if ch not in root.children:
+                root.children[ch] = TrieNode()
+            root = root.children[ch]
             root.sentences[sentence] += count
         
 
