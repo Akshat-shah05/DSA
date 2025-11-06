fb1890cfe512f2b8e8f59d34c3376363a7b923b0
Akshat Shah <akshatshah1605@gmail.com>
2025-11-05 18:15:38 -0800

    Time: 3738 ms (40.56%) | Memory: 79.7 MB (17.51%) - LeetSync

--- 1142-minimum-knight-moves/minimum-knight-moves.py
+++ 1142-minimum-knight-moves/minimum-knight-moves.py
@@ -1,25 +1,23 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        directions = [(1, 2), (1, -2), (2, 1), (-2, 1), (-1, 2), (-1, -2), (-2, -1), (2, -1)]
+        directions = [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+        visited = set([(0, 0)])
         steps = 0
         q = deque([(0, 0)])
-        visited = set([(0, 0)])
 
         while q:
             for _ in range(len(q)):
                 curX, curY = q.popleft()
                 if (curX, curY) == (x, y):
                     return steps
-
+                
                 for dx, dy in directions:
                     newX, newY = curX + dx, curY + dy
-                    if (newX, newY) in visited:
-                        continue
-                    
-                    visited.add((newX, newY))
-                    q.append((newX, newY))
-                
+                    if (newX, newY) not in visited:
+                        visited.add((newX, newY))
+                        q.append((newX, newY))
+
+            
             steps += 1
         
-        return steps
-            
+        return -1
