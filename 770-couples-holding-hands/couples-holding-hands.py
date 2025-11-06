d8d91375041d4a5e48685226beb7c0820d8cefac
Akshat Shah <akshatshah1605@gmail.com>
2025-11-05 19:18:31 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (72.64%) - LeetSync

--- 770-couples-holding-hands/couples-holding-hands.py
+++ 770-couples-holding-hands/couples-holding-hands.py
@@ -1,5 +1,6 @@
 class Solution:
     def minSwapsCouples(self, row: List[int]) -> int:
+        pos = {v : i for i, v in enumerate(row)}
         ans = 0
         for i in range(0, len(row), 2):
             x = row[i]
@@ -7,11 +8,11 @@ class Solution:
             if correct: continue
 
             ans += 1
-            for j in range(i + 1, len(row)):
-                correct = row[j] == x ^ 1
-                if correct:
-                    row[i + 1], row[j] = row[j], row[i + 1]
-                    break
-                
-        
+            partnerIdx = pos[x ^ 1]
+            movedVal = row[i + 1]
+
+            row[i + 1], row[partnerIdx] = row[partnerIdx], row[i + 1]
+            pos[x ^ 1] = i + 1
+            pos[movedVal] = partnerIdx
+
         return ans
