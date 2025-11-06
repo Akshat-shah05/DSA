61448dc5c7ca954224b6b0c104f26c50abd4bde9
Akshat Shah <akshatshah1605@gmail.com>
2025-11-05 19:15:05 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (72.64%) - LeetSync

--- 770-couples-holding-hands/couples-holding-hands.py
+++ 770-couples-holding-hands/couples-holding-hands.py
@@ -5,11 +5,13 @@ class Solution:
             x = row[i]
             correct = row[i + 1] == x + 1 if x % 2 == 0 else row[i + 1] == x - 1
             if correct: continue
-            ans +=1
+
+            ans += 1
             for j in range(i + 1, len(row)):
                 correct = row[j] == x + 1 if x % 2 == 0 else row[j] == x - 1
                 if correct:
                     row[i + 1], row[j] = row[j], row[i + 1]
                     break
-
+                
+        
         return ans
