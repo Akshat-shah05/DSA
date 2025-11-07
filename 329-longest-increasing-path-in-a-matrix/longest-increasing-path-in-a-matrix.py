764867d4546542f791a0afd983c838113d505ff1
Akshat Shah <akshatshah1605@gmail.com>
2025-11-06 19:32:07 -0800

    Time: 187 ms (32.09%) | Memory: 23.2 MB (16.69%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,31 +1,31 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
+        directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
         cache = {}
         maxLength = 0
 
-        def dfs(r, c, length):
+        def dfs(r, c):
             if not (0 <= r < rows) or not (0 <= c < cols):
                 return 0
-            
+
             if (r, c) in cache:
                 return cache[(r, c)]
- 
-            ans = 0
+
+            ans = 1
 
             for dr, dc in directions:
                 newR, newC = r + dr, c + dc
-                if (0 <= newR < rows) and (0 <= newC < cols) and matrix[newR][newC] > matrix[r][c]:
-                    ans = max(ans, dfs(newR, newC, length + 1))
+                if 0 <= newR < rows and 0 <= newC < cols and matrix[newR][newC] > matrix[r][c]:
+                   ans = max(ans, dfs(newR, newC) + 1)
 
-            cache[(r, c)] = ans + 1
-            return cache[(r, c)] 
+            cache[(r, c)] = ans
+            return ans
         
         for r in range(rows):
             for c in range(cols):
-                maxLength = max(maxLength, dfs(r, c, 0))
+                maxLength = max(maxLength, dfs(r, c))
         
         return maxLength
 
+                
