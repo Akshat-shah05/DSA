58f6f181cee86e6b4e600aad1ff8e3b0dcd7d454
Akshat Shah <akshatshah1605@gmail.com>
2025-11-07 18:43:02 -0800

    Time: 3 ms (93.27%) | Memory: 19.3 MB (65.43%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -3,21 +3,15 @@ class Solution:
         res = []
         n = len(graph)
         target = n - 1
-        seen = set()
 
         def dfs(node, path):
             if node == target:
                 res.append(path[:])
 
             for nei in graph[node]:
-                if nei in seen:
-                    continue
-                
-                seen.add(nei)
                 path.append(nei)
                 dfs(nei, path)
                 path.pop()
-                seen.remove(nei)
-        
+                    
         dfs(0, [0])
         return res
