810bbb72d863e1951aecae1e6f5cf75b31584221
Akshat Shah <akshatshah1605@gmail.com>
2025-11-07 23:13:14 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.87%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev2, prev1 = 0, 0
+        # Smallest Case
+            # If one house --> max value is nums[0]
+            # If two houses --> max value is max(nums[1], nums[0])
+            # IF > 2 houses 
+                # For any house i with value v, its max value is 
+                    #  max(v + (max value for house i - 2), (max value for house i - 1))
+        if not nums:
+            return 0
 
-        for num in nums:
-            temp = max(num + prev2, prev1)
-            prev2 = prev1
-            prev1 = temp
+        if len(nums) == 1:
+            return nums[0]
         
-        return prev1
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
         
+        dp = [0] * (len(nums) + 1)
+        dp[1] = nums[0]
+        dp[2] = max(nums[0], nums[1])
+
+        for i in range(3, len(nums) + 1):
+            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2])
+        
+        return dp[-1]
