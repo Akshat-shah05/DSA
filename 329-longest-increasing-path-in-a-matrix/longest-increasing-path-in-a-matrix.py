a0da4bcc1108e9efa68d9cf32fbdbad75e5bb3e4
Akshat Shah <akshatshah1605@gmail.com>
2025-11-07 22:12:43 -0800

    Time: 177 ms (40.07%) | Memory: 23.4 MB (14.15%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -2,30 +2,27 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
         directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        LIS = 0
         cache = {}
-        maxLength = 0
 
-        def dfs(r, c):
-            if not (0 <= r < rows) or not (0 <= c < cols):
-                return 0
-
+        def backtrack(r, c):
             if (r, c) in cache:
                 return cache[(r, c)]
 
+            if not (0 <= r < rows) or not (0 <= c < cols):
+                return 0
+            
             ans = 1
-
             for dr, dc in directions:
                 newR, newC = r + dr, c + dc
                 if 0 <= newR < rows and 0 <= newC < cols and matrix[newR][newC] > matrix[r][c]:
-                   ans = max(ans, dfs(newR, newC) + 1)
+                    ans = max(ans, 1 + backtrack(newR, newC))
 
             cache[(r, c)] = ans
             return ans
         
         for r in range(rows):
             for c in range(cols):
-                maxLength = max(maxLength, dfs(r, c))
+                LIS = max(LIS, backtrack(r, c))
         
-        return maxLength
-
-                
+        return LIS
