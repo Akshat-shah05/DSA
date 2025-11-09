958f978ee09e7b9249396250640f49d1463e9522
Akshat Shah <akshatshah1605@gmail.com>
2025-11-09 12:09:52 -0800

    Time: 161 ms (42.21%) | Memory: 43.6 MB (76.81%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.py
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -1,26 +1,28 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        # For any DAG, theres at least one node of indegree 0
+        # Time for course = base time for course + max(time for parents)
+
         adjList = defaultdict(list)
         parentList = defaultdict(int)
         indegrees = [0] * (n + 1)
-        for prereq, postreq in relations:
-            adjList[prereq].append(postreq)
-            indegrees[postreq] += 1
+        for u, v in relations:
+            adjList[u].append(v)
+            indegrees[v] += 1
         
-        maxTime = 0
-        q = deque([(x, time[x - 1]) for x in range(1, n + 1) if indegrees[x] == 0])
+        q = deque([(i, time[i - 1]) for i in range(1, n + 1) if indegrees[i] == 0])
+
+        timeTaken = 0
 
         while q:
-            course, t = q.popleft()
-            maxTime = max(maxTime, t)
+            course, timeSoFar = q.popleft()
+            timeTaken = max(timeTaken, timeSoFar)
 
             for nei in adjList[course]:
-                parentList[nei] = max(parentList[nei], t)
+                parentList[nei] = max(parentList[nei], timeSoFar)
                 indegrees[nei] -= 1
                 if indegrees[nei] == 0:
                     q.append((nei, time[nei - 1] + parentList[nei]))
-
-        return maxTime
         
+        return timeTaken
 
-        return maxTime
