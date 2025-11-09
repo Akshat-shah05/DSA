b684eead2b423579c7c04ad2ec0c4847bd50aa59
Akshat Shah <akshatshah1605@gmail.com>
2025-11-09 15:19:54 -0800

    Time: 3 ms (93.39%) | Memory: 19.3 MB (32.69%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,17 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        res = []
         n = len(graph)
-        target = n - 1
+        paths = []
 
-        def dfs(node, path):
-            if node == target:
-                res.append(path[:])
+        def dfs(src, path):
+            nonlocal paths
+            if src == n - 1:
+                paths.append(path[:])
+                return
+            
+            for nei in graph[src]:
+                dfs(nei, path + [nei])
 
-            for nei in graph[node]:
-                path.append(nei)
-                dfs(nei, path)
-                path.pop()
-                    
         dfs(0, [0])
-        return res
+        return paths
