6f1d3d30fa97aadec3cedc7565549428fc745396
Akshat Shah <akshatshah1605@gmail.com>
2025-11-09 14:51:07 -0800

    Time: 4 ms (19.90%) | Memory: 18.8 MB (69.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -3,7 +3,6 @@ class Solution:
         ans = 0
 
         for i in range(1, len(prices)):
-            if prices[i] > prices[i - 1]:
-                ans += prices[i] - prices[i - 1]
+            ans += max(prices[i] - prices[i - 1], 0)
         
         return ans
