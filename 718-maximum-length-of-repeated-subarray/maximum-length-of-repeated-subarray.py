69341f334628c6b7c172cb5cc0f994de00e82ca7
Akshat Shah <akshatshah1605@gmail.com>
2025-11-09 10:59:52 -0800

    Time: 836 ms (86.76%) | Memory: 42.7 MB (39.56%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,14 +1,15 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        # let dp[i][j] be the length of the longest common prefix of the suffixes A[i:], B[i:]
-        # then if A[i] == B[j], we know that dp[i][j] = dp[i + 1][j + 1] + 1
-        A, B = nums1, nums2
+        # Every subarray is a prefix of a suffix 
+        # Let dp[i][j] represent the longest common prefix of the suffix nums1[i:], nums2[j:]
 
-        dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        # dp[i][j] = dp[i + 1][j + 1] + 1
 
-        for i in range(len(A) - 1, -1, -1):
-            for j in range(len(B) - 1, -1, -1):
-                if A[i] == B[j]:
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        
+        for i in range(len(nums1) - 1, -1, -1):
+            for j in range(len(nums2) - 1, -1, -1):
+                if nums1[i] == nums2[j]:
                     dp[i][j] = dp[i + 1][j + 1] + 1
         
         return max(max(row) for row in dp)
