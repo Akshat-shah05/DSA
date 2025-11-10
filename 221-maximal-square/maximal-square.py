de636bcd17f3511802acf36d5e0b2a46f23bf656
Akshat Shah <akshatshah1605@gmail.com>
2025-11-09 17:39:44 -0800

    Time: 117 ms (86.82%) | Memory: 33 MB (76.29%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,17 +1,19 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        rows, cols = len(matrix), len(matrix[0])
+        # DP[i][j] will represent the maximum sidelength of a square whose bottom corner is (i, j)
+        # DP[i][j] = min(DP[i - 1][j], DP[i - 1][j - 1], DP[i][j - 1]) + 1
 
+        rows, cols = len(matrix), len(matrix[0])
         dp = [[0] * (cols + 1) for _ in range(rows + 1)]
-        best = 0
+
+        maxsqlen = 0
 
         for i in range(1, rows + 1):
             for j in range(1, cols + 1):
                 if matrix[i - 1][j - 1] == "1":
-                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
-                    best = max(best, dp[i][j])
+                    dp[i][j] = min (
+                        dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]
+                    ) + 1
+                    maxsqlen = max(maxsqlen, dp[i][j])
         
-        return best**2
-
-         
-        
+        return maxsqlen**2
