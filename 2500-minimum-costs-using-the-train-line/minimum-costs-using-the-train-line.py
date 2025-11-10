05bc621a55a602e2917049d827ec4ba857d97268
Akshat Shah <akshatshah1605@gmail.com>
2025-11-09 18:38:19 -0800

    Time: 127 ms (90.71%) | Memory: 37.8 MB (78.14%) - LeetSync

--- 2500-minimum-costs-using-the-train-line/minimum-costs-using-the-train-line.py
+++ 2500-minimum-costs-using-the-train-line/minimum-costs-using-the-train-line.py
@@ -3,16 +3,15 @@ class Solution:
         # lets min(dp[i][0], dp[i][1]) represent minimum cost to get to stop i
         costs = []
         n = len(regular)
-        dp = [[0, 0] for _ in range(n + 1)]
-        dp[0][0] = 0
-        dp[0][1] = expressCost
+        baseRegular = 0
+        baseExpress = expressCost
 
         for i in range(1, n + 1):
-            dp[i][0] = regular[i - 1] + min(dp[i - 1][0], dp[i - 1][1])
-            dp[i][1] = express[i - 1] + min(dp[i - 1][0] + expressCost, dp[i - 1][1])
+            temp = baseRegular
+            baseRegular = regular[i - 1] + min(baseRegular, baseExpress)
+            baseExpress = express[i - 1] + min(temp + expressCost, baseExpress)
 
-            costs.append(min(dp[i][0], dp[i][1]))
+            costs.append(min(baseRegular, baseExpress))
         
         return costs
-
 
