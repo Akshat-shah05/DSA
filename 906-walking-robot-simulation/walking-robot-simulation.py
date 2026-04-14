da97c6188e2243ed0a636cc39f84aa147d163a79
Akshat Shah <akshatshah1605@gmail.com>
2026-04-14 16:28:42 -0400

    Time: 27 ms (83.03%) | Memory: 23.9 MB (26.24%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -4,11 +4,22 @@ class Solution:
         facing = "N"
         left_map = {"N":"W", "W":"S", "S":"E", "E":"N"}
         right_map = {"N":"E", "E":"S", "S":"W", "W":"N"}
+        updates = {"N":(0, 1), "S":(0, -1), "E":(1, 0), "W":(-1, 0)}
         obstacle_o1 = defaultdict(set)
         max_distance = 0
 
         for x1, y1 in obstacles:
             obstacle_o1[x1].add(y1)
+        
+        def move(x_update, y_update, c):
+            nonlocal x
+            nonlocal y
+            for i in range(c):
+                if x + x_update in obstacle_o1 and y + y_update in obstacle_o1[x + x_update]:
+                    break
+                
+                x += x_update
+                y += y_update
         
         for c in commands:
             if c == -2:
@@ -20,34 +31,8 @@ class Solution:
                 continue
             
             else:
-                if facing == "N":
-                    for i in range(c):
-                        if x in obstacle_o1 and (y + 1) in obstacle_o1[x]:
-                            break
-                        
-                        y += 1
-                
-                elif facing == "E":
-                    for i in range(c):
-                        if (x + 1) in obstacle_o1 and y in obstacle_o1[x + 1]:
-                            break
-                        
-                        x += 1
-                
-                elif facing == "S":
-                    for i in range(c):
-                        if x in obstacle_o1 and (y - 1) in obstacle_o1[x]:
-                            break
-                        
-                        y -= 1
-                
-                else:
-                    for i in range(c):
-                        if (x - 1) in obstacle_o1 and y in obstacle_o1[x - 1]:
-                            break
-                        
-                        x -= 1
-
+                x_u, y_u = updates[facing]
+                move(x_u, y_u, c)
                 max_distance = max(max_distance, x**2 + y**2)
         
         return max_distance
