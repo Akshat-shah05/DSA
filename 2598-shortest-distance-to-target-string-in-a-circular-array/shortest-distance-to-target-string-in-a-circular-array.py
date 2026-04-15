3c4223cc33b2912b52a8970c627aa0ffce2ac213
Akshat Shah <akshatshah1605@gmail.com>
2026-04-15 01:11:49 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.69%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
@@ -3,12 +3,11 @@ class Solution:
         l, r = startIndex, startIndex
         n = len(words)
         steps = 0
-        while words[l] != target and words[r] != target and not (steps > 0 and l == r):
+        while steps <= n:
+            if words[l] == target or words[r] == target:
+                return steps
             l = (l - 1 + n) % n
             r = (r + 1) % n
             steps += 1
-        
-        if words[l] == target or words[r] == target:
-            return steps
         
         return -1
