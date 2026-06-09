6c833cd9adf066afc4978bf62dc72d20a3d2523b
Akshat Shah <akshatshah1605@gmail.com>
2026-06-08 22:46:45 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (92.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,23 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # Strat: put opening brackets on the stack, match w closing brackets
+        allowed = {")":"(", "]":"[", "}":"{"}
         stack = []
-        matches = {")" : "(", "}" : "{", "]": "["}
 
-        for par in s:
-            if not stack and par in matches:
-                return False
-            
-            elif par not in matches:
-                stack.append(par)
-            
-            else:
-                recent = stack[-1]
-                if matches[par] == recent:
-                    stack.pop()
-                
-                else:
-                    return False
+        for char in s:
+            if char not in allowed:
+                stack.append(char)
         
-        return True if stack == [] else False
+            elif stack and allowed[char] == stack[-1]:
+                stack.pop()
+
+            else:
+                return False
+        
+        return True if not stack else False
         
