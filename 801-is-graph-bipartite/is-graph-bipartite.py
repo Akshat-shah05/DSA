edffc95d03f99493cbe0a5f422796e4d79e97338
Akshat Shah <akshatshah1605@gmail.com>
2026-07-11 16:43:14 -0700

    Time: 2 ms (39.81%) | Memory: 19.9 MB (36.81%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,31 +1,26 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        adj_list = defaultdict(list)
-        for u in range(len(graph)):
-            adj_list[u] = graph[u][:]
+        n = len(graph)
+        colors = [-1] * n
 
-        seen = {}
-        def check(adj_list, start, seen):
-            q = deque([(start, 1)])
-            while len(q) > 0:
-                pop, color = q.popleft()
-                if pop in seen:
-                    if seen[pop] != color:
+        for start in range(n):
+            if colors[start] != -1:
+                continue
+
+            colors[start] = 0
+            q = deque([start])
+
+            while q:
+                node = q.popleft()
+
+                for neighbor in graph[node]:
+                    if colors[neighbor] == -1:
+                        colors[neighbor] = 1 - colors[node]
+                        q.append(neighbor)
+                    
+                    elif colors[neighbor] == colors[node]:
                         return False
-                    continue
-                
-                seen[pop] = color
-                vertices = graph[pop]
-                for v in vertices:
-                    q.append((v, -color))
             
-            return True
-
-        for i in range(len(graph)):
-            if i not in seen:
-                if check(adj_list, i, seen) == False:
-                    return False
-        
         return True
 
      
