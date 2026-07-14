36d4dcffa08daf3fbf0c627042d55ab5e80f0fec
Akshat Shah <akshatshah1605@gmail.com>
2026-07-13 17:47:00 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (16.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,12 +3,17 @@ class Solution:
         if n == 1:
             return 1
         
-        prev2 = 1
-        prev1 = 2
+        if n == 2:
+            return 2
         
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        # dp[i] is # of distinct ways to get to stair i
+        # dp[i] = dp[i - 1] + dp[i - 2]
+
         for i in range(3, n + 1):
-            temp = prev1
-            prev1 = prev1 + prev2
-            prev2 = temp
+            dp[i] = dp[i - 1] + dp[i - 2]
         
-        return prev1
+        return dp[n]
