5725e8070a87b7c0f699b16a1cf07e5875959f49
Akshat Shah <akshatshah1605@gmail.com>
2026-07-13 18:26:57 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.97%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,25 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # Smallest Case
-            # If one house --> max value is nums[0]
-            # If two houses --> max value is max(nums[1], nums[0])
-            # IF > 2 houses 
-                # For any house i with value v, its max value is 
-                    #  max(v + (max value for house i - 2), (max value for house i - 1))
-        if not nums:
+        # dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        # let dp[i] represent the maximum amount of money you can make from houses 0 to i
+        #   dp[0] = 0 // robbing no houses
+        #   dp[1] = 1 // robbing 1 house
+        #   dp[2] = max(dp[0], dp[1])
+        #   dp[3] = max(dp[1] + nums[3], dp[2])
+        n = len(nums)
+        if n == 0:
             return 0
-
-        if len(nums) == 1:
+        
+        if n == 1:
             return nums[0]
-        
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-        
-        dp = [0] * (len(nums) + 1)
+
+        dp = [0] * (n + 1)
         dp[1] = nums[0]
         dp[2] = max(nums[0], nums[1])
 
-        for i in range(3, len(nums) + 1):
-            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2])
+        for i in range(3, n + 1):
+            dp[i] = max(dp[i - 2] + nums[i - 1], dp[i - 1])
         
-        return dp[-1]
+        return dp[n]
+
