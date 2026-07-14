bf16da07cc9e86739f8a1bdcb2648678567aa620
Akshat Shah <akshatshah1605@gmail.com>
2026-07-13 18:14:27 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (41.85%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,4 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m + n - 2, n - 1)
+        # number of ways to get to square (r, c) is 
+            # number of ways to get to square (r, c - 1) + (r - 1, c)
+        
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[1][1] = 1
+
+        for r in range(1, m + 1):
+            for c in range(1, n + 1):    
+                if (r, c) == (1, 1):
+                    continue
+                
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+        
+        return dp[m][n]
 
